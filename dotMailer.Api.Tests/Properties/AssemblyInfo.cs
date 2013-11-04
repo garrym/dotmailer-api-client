5f0248215dc2f7c8a8265ab5273b43a301a24828
garrym <garry@basefour.co.uk>
2013-11-04 15:30:35 +0000

    Added solution folder

--- dotMailer.Api.Tests/Properties/AssemblyInfo.cs
+++ dotMailer.Api.Tests/Properties/AssemblyInfo.cs
@@ -1,18 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("dotMailer.Api.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("DOTDIGITALGROUP")]
 [assembly: AssemblyProduct("dotMailer.Api.Tests")]
-[assembly: AssemblyCopyright("Copyright © DOTDIGITALGROUP 2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
